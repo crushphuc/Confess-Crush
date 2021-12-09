09c4095c32a932feab8d4f743eae0b1397ab0566
crushphuc <93134858+crushphuc@users.noreply.github.com>
2021-12-09 16:58:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "GLST-JOKER",
-    introTitle: 'Nguyễn Văn Linh',
+    titleWeb: "Anh yêu Em",
+    introTitle: 'GLST-JOKER',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
